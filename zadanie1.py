bfda959920c4210da6743b1a0cec9cbb924168dc
Kotleciq <wiktor_siedlanowski@outlook.com>
2022-10-25 17:53:40 +0200

    inital commit

---
+++ zadanie1.py
@@ -0,0 +1,15 @@
+print('program 8')
+x=int(input('podaj liczbe x'))
+y=int(input('podaj liczbe y'))
+print(x, "+" ,y, "=", x+y)
+print(x, "-" ,y, "=", x-y)
+print(x, "*" ,y, "=", x*y)
+print(x, "/" ,y, "=", x/y)
+print(x, "//",y, "=", x//y)
+print(x, "%" ,y, "=", x%y)
+print(x, "**" ,y, "=", x**y)
+
+print('program oblicza sume n kolejnych liczb naturalnych')
+n=int(input('podaj n'))
+k=n*(n+1)/2
+print('suma kolejnych',n,'liczb naturalnych wynosi', k)
---
+++ zadanie2.py
@@ -0,0 +1,26 @@
+import imp
+from math import*
+from tkinter import W
+# program 1
+a=float(input('podaj a'))
+b=float(input('podaj b'))
+w=sqrt(7/(a**3+cos(b)))
+print('wartosc wyrazenia',W)
+
+# program 2
+a=float(input('podaj a'))
+b=float(input('podaj b'))
+k=a**3+cos(b)*sqrt(a+b)
+print('wartosc wyrazenia',k)
+
+# program 3
+a=float(input('podaj a'))
+b=float(input('podaj b'))
+z=fabs(a-b) +sin(a) *sqrt(a+b)
+print('wartosc wyrazenia',z)
+
+# program 4
+a=float(input('podaj a'))
+b=float(input('podaj b'))
+h=(cos(a+1) /fabs(sqrt(5)-b) )
+print('wartosc wyrazenia',h)
